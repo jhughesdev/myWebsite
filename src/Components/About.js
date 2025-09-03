17d19a30101a0b075a4f516155fd894837e893e9
jacobhughes <jacobyh09@gmail.com>
2025-09-02 20:41:50 -0400

    fixing images

--- src/Components/About.js
+++ src/Components/About.js
@@ -15,9 +15,9 @@ function importAllImages(r) {
 }
 
 // Grab images from each category
-const travelImages = importAllImages(require.context('../Assets/Images/Carousel/Travel', false, /\.(png|jpeg|svg|jpg)$/));
-const foodImages = importAllImages(require.context('../Assets/Images/Carousel/Food', false, /\.(png|jpeg|svg|jpg)$/));
-const sportsImages = importAllImages(require.context('../Assets/Images/Carousel/Sports', false, /\.(png|jpeg|svg|jpg)$/));
+const travelImages = importAllImages(require.context('../Assets/Images/Carousel/Travel', false, /\.(png|jpeg|svg)$/));
+const foodImages = importAllImages(require.context('../Assets/Images/Carousel/Food', false, /\.(png|jpeg|svg)$/));
+const sportsImages = importAllImages(require.context('../Assets/Images/Carousel/Sports', false, /\.(png|jpeg|svg)$/));
 
 function CarouselSection({ title, images, description }) {
   return (
